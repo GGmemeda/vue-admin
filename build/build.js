2e9c045822e30852513db7b217d191d04f8649a9
LAPTOP <caobinhen6@gmail.com>
2018-01-08 18:37:39 +0800

    改了下端口

--- build/build.js
+++ build/build.js
@@ -30,7 +30,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     console.log(chalk.cyan('  Build complete.\n'));
     if(process.env.build_preview){
       server.start({
-        port: 9528,
+        port: 8001,
         directory: './dist',
         file: '/index.html'
       });
