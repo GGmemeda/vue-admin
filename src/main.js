16a9b6d0d7c8bdfc974b17e00819c6dda57668c8
LAPTOP <caobinhen6@gmail.com>
2018-02-26 17:15:07 +0800

    更改部分

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@
 import Vue from 'vue';
 import App from './App';
 import Vuex from 'vuex';
-import { createRouter } from './router';
-import { createStore } from './store';
+import router from './router';
+import store from './store';
 import 'font-awesome/css/font-awesome.css';
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
@@ -14,16 +14,11 @@ Vue.use(iView);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-export function createApp () {
-  const router = createRouter();
-  const store = createStore();
-  const app = new Vue({
-    el: '#app',
-    router,
-    store,
-    template: '<App/>',
-    components: { App }
-  });
-  return { app, router, store };
-}
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+});
 
