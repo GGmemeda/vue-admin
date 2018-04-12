a1fa8a1411ff4f7072ae77f0f5f0c8f738ecc36f
DESKTOP-RFBGN5A\caobin <caobinhen6@gmail.com>
2018-04-12 22:44:05 +0800

    修改东西

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -38,6 +38,24 @@ module.exports = merge(baseWebpackConfig, {
       inject: true
     }),
     new FriendlyErrorsPlugin()
-  ]
+  ],
+  devServer: {
+    clientLogLevel: 'warning',
+    historyApiFallback: true,
+    hot: true,
+    compress: true,
+    host: HOST || config.dev.host,
+    port: PORT || config.dev.port,
+    open: config.dev.autoOpenBrowser,
+    overlay: config.dev.errorOverlay
+      ? { warnings: false, errors: true }
+      : false,
+    publicPath: config.dev.assetsPublicPath,
+    proxy: config.dev.proxyTable,
+    quiet: true, // necessary for FriendlyErrorsPlugin
+    watchOptions: {
+      poll: config.dev.poll,
+    }
+  }
 });
 
